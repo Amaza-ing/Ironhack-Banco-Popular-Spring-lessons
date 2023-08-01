2ad378181bc7960e46b9304602fd6445c8bca0ae
Adrián Maza <>
2023-08-01 17:53:37 +0200

    PUT PATCH DELETE requests added

--- w4d2/src/main/java/com/ironhack/w4d2/service/interfaces/ICourseService.java
+++ w4d2/src/main/java/com/ironhack/w4d2/service/interfaces/ICourseService.java
@@ -11,4 +11,12 @@ public interface ICourseService {
     public Course getCourseById(String course);
 
     List<Course> getCourseByClassroomAndHours(String classroom, Optional<Integer> hours);
+
+    void updateCourse(Course course, String id);
+
+    void updateCourseHours(Integer hours, String id);
+
+    void updateCourseClassroom(String classroom, String id);
+
+    void deleteCourse(String id);
 }
