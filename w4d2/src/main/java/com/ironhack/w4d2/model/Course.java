af68cddf665baa077daf93891d5face2fc77c957
Adrián Maza <>
2023-08-01 16:25:47 +0200

    service layer & POST request added

--- w4d2/src/main/java/com/ironhack/w4d2/model/Course.java
+++ w4d2/src/main/java/com/ironhack/w4d2/model/Course.java
@@ -1,6 +1,9 @@
 package com.ironhack.w4d2.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +15,14 @@ import lombok.NoArgsConstructor;
 public class Course {
     @Id
     private String course;
+
+    @Max(value = 400, message = "The course cannot be longer than 400 hours")
+    @Min(50)
     private Integer hours;
+
+    @NotEmpty(message = "Classroom cannot be empty")
     private String classroom;
+
     private String vacations;
 
     @ManyToOne
