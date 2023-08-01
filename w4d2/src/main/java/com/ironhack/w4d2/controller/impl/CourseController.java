2442b3ec476d64005d5f146ffb63420d36965998
Adrián Maza <>
2023-08-01 12:23:37 +0200

    w4d2 added

---
+++ w4d2/src/main/java/com/ironhack/w4d2/controller/impl/CourseController.java
@@ -0,0 +1,48 @@
+package com.ironhack.w4d2.controller.impl;
+
+import com.ironhack.w4d2.controller.interfaces.ICourseController;
+import com.ironhack.w4d2.model.Course;
+import com.ironhack.w4d2.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class CourseController implements ICourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping("/courses")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @GetMapping("/courses/{course}")
+    public Course getCourseById(@PathVariable(name = "course") String course) {
+        Optional<Course> courseOptional = courseRepository.findById(course);
+        if (courseOptional.isEmpty()) return null;
+        return courseOptional.get();
+    }
+
+    @GetMapping("/courses/hours")
+    public List<Course> getCourseByHoursLessThan(@RequestParam(defaultValue = "100") Integer hours) {
+        return courseRepository.findAllByHoursLessThan(hours);
+    }
+
+    @GetMapping("/courses/classroom")
+    public List<Course> getCourseByClassroomAndHours(
+            @RequestParam(defaultValue = "A1") String classroom,
+            @RequestParam Optional<Integer> hours
+    ) {
+        if (hours.isPresent()) return courseRepository.findAllByClassroomAndHours(classroom, hours.get());
+        return courseRepository.findAllByClassroom(classroom);
+    }
+
+
+}
