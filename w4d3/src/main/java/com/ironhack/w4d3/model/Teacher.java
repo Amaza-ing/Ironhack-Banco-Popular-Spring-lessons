5f33e5038f12d68cc23073f7f497ec212ee7f35b
Adrián Maza <>
2023-08-02 13:18:59 +0200

    w4d3 added

---
+++ w4d3/src/main/java/com/ironhack/w4d3/model/Teacher.java
@@ -0,0 +1,53 @@
+package com.ironhack.w4d3.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String teacher;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+//    Use only if we want a bidirectional relationship
+//    If used, we must be careful not to create a stackOverflow Error (infinite loop)
+//    @OneToMany(mappedBy = "teacher")
+//    private List<Course> courses;
+
+
+    public Teacher(String teacher, Address address) {
+        this.teacher = teacher;
+        this.address = address;
+    }
+
+
+
+//    @Override
+//    public String toString() {
+//        return "Teacher{" +
+//                "id=" + id +
+//                ", teacher='" + teacher + '\'' +
+//                ", address=" + address +
+////                ", courses=" + getCoursesName() +
+//                '}';
+//    }
+
+//    private List<String> getCoursesName() {
+//        List<String> coursesName = new ArrayList<>();
+//
+//        for (Course course : courses) {
+//            coursesName.add(course.getCourse());
+//        }
+//
+//        return coursesName;
+//    }
+}
